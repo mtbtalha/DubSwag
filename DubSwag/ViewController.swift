1272abdc2b6490ea5f8c4f7bb9a8198865e61b97
mtb.talha <talha.babar@arbisoft.com.pk>
2015-07-30 20:16:49 +0500

    first commit

---
+++ DubSwag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DubSwag
+//
+//  Created by Talha Babar on 7/30/15.
+//  Copyright (c) 2015 Talha Babar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
